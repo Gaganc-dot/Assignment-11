424c87a1d2fe00eaffa8232455c412c9b5c63ec7
Gaganc-dot <gaganchoudhary213@gmail.com>
2025-06-18 11:16:59 +0530

    Update Assignment11_6.py

--- Assignment11_6.py
+++ Assignment11_6.py
@@ -1,1 +1,18 @@
 
+def natural(no1):
+     if no1 <= 1 :
+         return no1
+     return no1 + natural(no1-1)
+                
+
+def main():
+    print("Enter the your number :")
+    n = int(input())
+    
+    
+    Ans = natural(n)
+    print("Sum of natural no is :",Ans)
+   
+
+if __name__ == "__main__":
+    main()
